d888dc22b91f777431f5994f60766416d00f5ca0
Oliwier <oliwierrosiak07@gmail.com>
2024-08-27 23:51:57 +0200

    Firebase added

---
+++ js/database.js
@@ -0,0 +1,3 @@
+const database = `https://oliwier-rosiak-portfolio-default-rtdb.europe-west1.firebasedatabase.app/`
+
+export default database
---
+++ js/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfIjAWByAluH-VG7yjfzC1Yn4DVUvB9u4",
+  authDomain: "oliwier-rosiak-portfolio.firebaseapp.com",
+  projectId: "oliwier-rosiak-portfolio",
+  storageBucket: "oliwier-rosiak-portfolio.appspot.com",
+  messagingSenderId: "949024815396",
+  appId: "1:949024815396:web:e9203c0540449c15599bb1"
+};
+
+const app = initializeApp(firebaseConfig);
